9c1066bfa95db73dea7c50f2cee9975a485eabaf
patuo <nopattun15@gmail.com>
2019-06-19 10:00:05 +0000

    coding routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,25 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   get 'signup', to: 'users#new'
-  resources :user, only: [:show, :new, :create, :edit, :update]
+  
+  resources :user, except: [:index, :delete]
+  
+  resources :small_purpose, except: [:delete] do
+    collection do
+      get :completed
+    end 
+  end 
+  
+  resources :big_purpose, except: [:delete] do
+    collection do
+      get :completed
+    end
+    
+    member do
+      resources :small_purpose, only: [:index, :show, :completed]
+    end 
+  end 
+  
+  resources :time_report, only: [:new, :create, :edit, :update]
+  resources :growth_reports, only: [:new, :create, :edit, :update]
 end
